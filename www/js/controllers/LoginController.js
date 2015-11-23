857fe1f166f452d85250fcf92ff113e5717b696a
mxiamxia <mxiamxia@gmail.com>
2015-11-23 11:12:29 -0500

    add login error messages

--- www/js/controllers/LoginController.js
+++ www/js/controllers/LoginController.js
@@ -2,13 +2,28 @@ app.controller('LoginController',function($state,$sanitize, Login) {
 	var self=this;
   self.data = {};
 
+  self.loginerror = false;
+  self.errdetail;
+
+
   self.loginApp = function() {
     var username=$sanitize(self.data.username);
     var password = self.data.password;
     if(username && password)
     {
-      Login.login(username, password);
-      //$state.go('chat',{username:username})
+      Login.login(username, password).then(function (resp) {
+        if(resp.data.status.code == '0000') {
+          $state.go('chat',{username:username})
+        } else {
+          console.log('login fail');
+          self.loginerror = true;
+          self.errdetail = 'Please verify your user name and password';
+        }
+      }, function (err) {
+        console.log('login error');
+        self.loginerror = true;
+        self.errdetail = 'Fail to connect to remote server';
+      });
     }
     console.log("LOGIN user: " + username + " - PW: " + password);
   }
