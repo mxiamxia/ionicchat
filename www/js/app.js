22246ac3e8ba36ac1e223e93709b9c22f4b6c649
Min Xia <mxiamxia@gmail.com>
2015-11-30 22:23:05 -0500

    style change

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('co-engage', ['ionic', 'ngCordova', 'ngSanitize', 'co.engage.services'])
+var app = angular.module('co-engage', ['ionic', 'ngCordova', 'ngSanitize', 'btford.socket-io', 'co.engage.services'])
 
 .run(function($ionicPlatform, $cordovaTouchID, $state) {
   $ionicPlatform.ready(function() {
